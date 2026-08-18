1c90dab82078bba2090d51bbbdb5c70b31ed5211
Bobby Dias - O Capitão <renatogodias@gmail.com>
2026-08-18 01:42:20 -0300

    ui: switch chat header to Forty-Two quick panel

--- src/components/ChatHeader/ChatHeader.tsx
+++ src/components/ChatHeader/ChatHeader.tsx
@@ -5,7 +5,7 @@ import {observer} from 'mobx-react';
 import {createStyles} from './styles';
 import {HeaderRight} from '../HeaderRight';
 import {ChatHeaderTitle} from '../ChatHeaderTitle';
-import {EnterpriseQuickPanel} from '../EnterpriseQuickPanel';
+import {FortyTwoQuickPanel} from '../FortyTwoQuickPanel';
 import {
   useSafeAreaFrame,
   useSafeAreaInsets,
@@ -24,12 +24,10 @@ export const ChatHeader: React.FC = observer(() => {
   // On models with Dynamic Island the status bar height is smaller than the safe area top inset.
   // https://github.com/react-navigation/react-navigation/blob/e4815c538536ddccf4207b87bf3e2f1603dedd84/packages/elements/src/Header/Header.tsx#L52
   // NOTE: in v7, this is fixed and getDefaultHeaderHeight returns the correct height.
-
   const hasDynamicIsland = Platform.OS === 'ios' && insets.top > 50;
   const statusBarHeight = hasDynamicIsland ? insets.top - 5 : insets.top;
 
   const headerHeight = getDefaultHeaderHeight(layout, false, statusBarHeight);
-
   const styles = createStyles({theme, insets, headerHeight});
 
   const headerStyle = chatSessionStore?.shouldShowHeaderDivider
@@ -43,7 +41,7 @@ export const ChatHeader: React.FC = observer(() => {
         <ChatHeaderTitle />
       </View>
       <View style={styles.rightSection}>
-        <EnterpriseQuickPanel />
+        <FortyTwoQuickPanel />
         <HeaderRight />
       </View>
     </View>
